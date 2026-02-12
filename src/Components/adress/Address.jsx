3ec699245265a3625a6d17450fa02f19b7490656
Ismail Abdullah <ismailabdullah243@gmail.com>
2026-02-12 15:01:05 +0530

    Update Address component with red location icon

--- src/Components/adress/Address.jsx
+++ src/Components/adress/Address.jsx
@@ -1,19 +1,32 @@
-import CallButton from "./CallButton"
-import './Address.css'
-
+import CallButton from "./CallButton";
+import './Address.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 const Address = () => {
+  const address = "Opp Zeeshan Cafe, Besides Katora House, Golconda, Hyderabad, TG-08";
+  const mapLink = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(address)}`;
+
   return (
-    <><div className="visit">
-          <h2>Visit Our Clinic</h2>
-            <p>Opp Zeeshan Cafe, Besides Katora House Golconda Hyderabad TG-08</p>
-            <p>Open from 10.00am - 11.00pm</p>
-          
-    </div>
-    <CallButton/>
+    <>
+      <div className="visit">
+        <h2>Visit Our Clinic</h2>
+        <p>{address}</p>
+        <p>Open from 10.00am - 11.00pm</p>
+
+        {/* Red location icon */}
+        <a
+          href={mapLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="location-icon"
+        >
+          <FontAwesomeIcon icon={faLocationDot} />
+        </a>
+      </div>
+      <CallButton/>
     </>
-  )
+  );
 }
 
-export default Address
-
+export default Address;
