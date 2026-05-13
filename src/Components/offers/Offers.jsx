65a6612e5e37a4c4dd0ace39b297aed47b83be65
Ismail Abdullah <ismailabdullah243@gmail.com>
2026-05-13 13:02:28 +0530

    Update frontend components

--- src/Components/offers/Offers.jsx
+++ src/Components/offers/Offers.jsx
@@ -1,12 +1,11 @@
 import { useEffect, useState } from "react";
-import offers1 from "../Assets/offers.png";
-import offers2 from "../Assets/slide2.png";
-import offers3 from "../Assets/slide3.png";
-import offers4 from "../Assets/slide4.png";
-import offers5 from "../Assets/slide5.png";
+import offers1 from "../Assets/slide2.png";
+import offers2 from "../Assets/slide3.png";
+import offers3 from "../Assets/slide4.png";
+import offers4 from "../Assets/slide5.png";
 import "./Offers.css";
 
-const images = [offers1, offers2, offers3, offers4, offers5];
+const images = [offers1, offers2, offers3, offers4];
 
 const Offers = () => {
   const [current, setCurrent] = useState(0);
